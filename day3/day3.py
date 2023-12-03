6fd1e73477ec6a46d413495d523100f77ba65abf
Gard Inge Rosvold <gardir@ifi.uio.no>
2023-12-03 21:31:27 +0100

    Looked up a solution and found the `finditer` function instead of findall -- solved my life now when that gave the indexes!

--- day3/day3.py
+++ day3/day3.py
@@ -6,20 +6,16 @@ def part1(input_file):
     return sum(map(int, part_numbers.values()))
 
 
-def valid_numbers(line, number, index):
-    end = 0
-    checked_all = False
-    all_numbers = []
-    while not checked_all:
-        try:
-            start = line.index(number, end)
-            end = start + len(number)
-            within = start-1 <= index <= end
-            if within:
-                all_numbers.append(start)
-        except ValueError:
-            checked_all = True
-    return all_numbers
+def valid_numbers(number, index):
+    try:
+        start = number.start()
+        end = number.end()
+        within = start - 1 <= index <= end
+        if within:
+            return [start]
+    except ValueError:
+        pass
+    return []
 
 
 def explore(schematic, row_index, symbol_column):
@@ -29,26 +25,26 @@ def explore(schematic, row_index, symbol_column):
     if min_row < row_index:
         above_row_index = row_index - 1
         above_row = schematic[above_row_index]
-        above_row_numbers = re.findall(pattern, above_row)
+        above_row_numbers = re.finditer(pattern, above_row)
         for number in above_row_numbers:
-            columns = valid_numbers(above_row, number, symbol_column)
+            columns = valid_numbers(number, symbol_column)
             for column in columns:
-                numbers[(above_row_index, column)] = number
+                numbers[(above_row_index, column)] = number.group()
     if row_index < max_row:
         below_row_index = row_index + 1
         below_row = schematic[below_row_index]
-        below_row_numbers = re.findall(pattern, below_row)
+        below_row_numbers = re.finditer(pattern, below_row)
         for number in below_row_numbers:
-            columns = valid_numbers(below_row, number, symbol_column)
+            columns = valid_numbers(number, symbol_column)
             for column in columns:
-                numbers[(below_row_index, column)] = number
+                numbers[(below_row_index, column)] = number.group()
 
     row = schematic[row_index]
-    row_numbers = re.findall(pattern, row)
+    row_numbers = re.finditer(pattern, row)
     for number in row_numbers:
-        columns = valid_numbers(row, number, symbol_column)
+        columns = valid_numbers(number, symbol_column)
         for column in columns:
-            numbers[(row_index, column)] = number
+            numbers[(row_index, column)] = number.group()
     return numbers
 
 
@@ -58,7 +54,7 @@ def extract_part_numbers(schematic, verbose=False):
     symbols = set()
     for row_index, line in enumerate(schematic):
         for i, symbol in enumerate(line):
-            if symbol != '.':
+            if symbol != '.' and symbol != '\n':
                 try:
                     v = int(symbol)
                     numerics.add(v)
@@ -77,4 +73,4 @@ if __name__ == '__main__':
     inputfilename = 'input'
     with open(inputfilename) as f:
         answer = part1(f)
-    print(answer)
+    print(f"{answer} (=? 522726)")
