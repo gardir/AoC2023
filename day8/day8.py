422fbfefcfe24980afe66e792d4d10dc8c915fc7
Gard Inge Rosvold <gardir@ifi.uio.no>
2023-12-08 18:51:27 +0100

    day8, part 2. really messed up

--- day8/day8.py
+++ day8/day8.py
@@ -1,23 +1,70 @@
+from threading import Thread, Lock
 import re
 
-path_re = re.compile(r"(\w+) = \((\w+), (\w+)\)")
+path_re = re.compile(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)")
 
 real = 'input'
-exampel = 'example'
-direction = {'L': 0, 'R': 1}
+example = 'example2'
+directions = {'L': 0, 'R': 1}
 
-with open(real) as f:
-    instructions = f.readline().rstrip()
-    f.readline()
-    paths = {}
-    for line in f.readlines():
-        print(f"'{line}'")
-        path, left, right = re.match(path_re, line.rstrip()).groups()
-        paths[path] = (left, right)
-    print(paths)
-    current = 'AAA'
-    step = 0
-    while current != 'ZZZ':
-        current = paths[current][direction[instructions[step % len(instructions)]]]
-        step += 1
-    print(step)
+
+def check_steps(steps):
+    initial = set(steps[0])
+    for step in steps[1:]:
+        initial = initial.intersection(set(step))
+    return initial
+
+
+if __name__ == '__main__':
+    # info('main line')
+    with open(real) as f:
+        instructions = f.readline().rstrip()
+        f.readline()
+        paths = {}
+        all_starts = []
+        for line in f.readlines():
+            path, left, right = re.match(path_re, line.rstrip()).groups()
+            paths[path] = (left, right)
+            if path[-1] == 'A':
+                all_starts.append(path)
+        step = 0
+        print(f"{len(all_starts)} paths to run concurrently")
+        lock = Lock()
+        steps = [[] for _ in range(len(all_starts))]
+
+        def add_step(i, step):
+            lock.acquire()
+            try:
+                steps[i].append(step)
+                return check_steps(steps)
+            finally:
+                lock.release()
+
+        def explore_paths(i):
+            step = 0
+            start = all_starts[i]
+            path = start
+            cycler = [start]
+            while True:
+                direction = directions[instructions[step % len(instructions)]]
+                path = paths[path][direction]
+                cycler.append(path)
+                if path in cycler and len(cycler) > len(instructions):
+                    print(f"It took [{i}] {step} steps to reach a complete 'cycle': {steps[i]}, and cycle {cycler}")
+                    break
+                step += 1
+                if path[-1] == 'Z':
+                    if add_step(i, step):
+                        break
+                elif path == start:
+                    print(f"It took [{i}] {steps} steps to 'cycle': {steps[i]}")
+                    break
+
+        threads = []
+        for i in range(len(all_starts)):
+            p = Thread(target=explore_paths, args=(i,))
+            p.start()
+            threads.append(p)
+        for p in threads:
+            p.join()
+        print(f"It took '{check_steps(steps)}' steps to reach all ending in 'Z': {all_starts}")
